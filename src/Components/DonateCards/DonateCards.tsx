6ea9b88e3146499dd361eccc52b91a646e60f2e6
huanginch <huanginch329@gmail.com>
2024-06-02 16:33:09 +0800

    update donate card

--- src/Components/DonateCards/DonateCards.tsx
+++ src/Components/DonateCards/DonateCards.tsx
@@ -3,11 +3,11 @@ import './DonateCards.scss';
 function DonateCards({ link, img, cardName, text }: { link:string, img: string, cardName: string, text: string }) {
   return (
     <a target="_blank" href={link} className="donate-card flex hover:opacity-50 transition-all duration-500 ease-in-out mx-auto mb-[36px] md:mb-[80px] tablet:mb-[123px]">
-      <div className="md:w-[63px] lg:w-auto h-full flex flex-col items-center justify-center">
+      <div className="md:w-[63px] lg:w-[145px] h-full flex flex-col items-center justify-center">
         <div className="card-name bg-white border-[5px] tablet:border-[13px] border-third rounded-t-[81px] flex-grow">
           <h5 className="text-4xl tablet:text-[64px] leading-[54px] tablet:leading-[120px] text-primary pt-[21px] pb-[5px]">{cardName}</h5>
         </div>
-        <div className="card-bottom w-full h-[29px] tablet:h-[48px] bg-third"></div>
+        <div className="card-bottom md:w-[63px] lg:w-[145px] h-[29px] tablet:h-[48px] bg-third"></div>
       </div>
       <div className="flex flex-col">
         <img className="w-[132px] h-[132px] mr-[29px] tablet:mr-[58px] tablet:w-[250px] tablet:h-[250px]" src={img} alt={cardName} />
