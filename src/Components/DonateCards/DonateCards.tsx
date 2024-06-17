1377cc9d25fe3f20d17a39b1eec693f42a3c62d3
huanginch <huanginch329@gmail.com>
2024-06-18 02:12:39 +0800

    @fix: donate card on android and ios

--- src/Components/DonateCards/DonateCards.tsx
+++ src/Components/DonateCards/DonateCards.tsx
@@ -1,19 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import './DonateCards.scss';
 
 function DonateCards({ link, img, cardName, text }: { link:string, img: string, cardName: string, text: string }) {
-  const isMac = /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgent)
+  const cardNameRef = useRef<HTMLDivElement>(null)
+
+  function iOS() {
+    return [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ].includes(navigator.platform)
+      // iPad on iOS 13 detection
+      || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  }
 
   useEffect(() => {
-    if (isMac) {
-      document.documentElement.classList.add("apple-device")
+    if (iOS()) {
+      //add larger line height for iOS devices
+      cardNameRef.current?.classList.add('ios')
+      console.log('Apple device detected')
     }
-  }, [isMac])
+  }, [])
 
   return (
     <a target="_blank" href={link} className="donate-card flex hover:opacity-50 transition-all duration-500 ease-in-out mx-auto mb-[36px] md:mb-[80px] tablet:mb-[123px]">
       <div className="w-[65px] md:w-[63px] lg:w-[145px] h-full flex flex-col items-center justify-center">
-        <div className='w-[65px] md:w-[63px] lg:w-[145px] flex-grow card-name bg-white border-[5px] tablet:border-[13px] border-third rounded-t-[81px]'>
+        <div ref={cardNameRef} className='w-[65px] md:w-[63px] lg:w-[145px] flex-grow card-name bg-white border-[5px] tablet:border-[13px] border-third rounded-t-[81px]'>
           <p className="text-4xl leading-[55px] tablet:text-[64px] lg:leading-[120px] text-primary pt-[21px] pb-[5px]">{cardName}</p>
         </div>
         <div className="card-bottom w-[65px] md:w-[63px] lg:w-[145px] h-[29px] tablet:h-[48px] bg-third"></div>
