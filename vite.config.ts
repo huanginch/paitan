cba38129a823bfcbcaca3c7bd07eb336a712ede5
huanginch <huanginch329@gmail.com>
2024-06-17 23:00:35 +0800

    @update: sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,7 +10,7 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env.YOUTUBE_API_KEY': JSON.stringify(env.YOUTUBE_API_KEY)
     },
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), Sitemap()],
     base: process.env.NODE_ENV === 'production' ? '/paitan/' : '/',
   }
 })
