90cb5b9fba23c147ba896349744d07e2f636cd15
huanginch <huanginch329@gmail.com>
2024-06-18 01:00:46 +0800

    @update: donate card

--- src/Components/DonateCards/DonateCards.tsx
+++ src/Components/DonateCards/DonateCards.tsx
@@ -4,9 +4,7 @@ function DonateCards({ link, img, cardName, text }: { link:string, img: string, 
   return (
     <a target="_blank" href={link} className="donate-card flex hover:opacity-50 transition-all duration-500 ease-in-out mx-auto mb-[36px] md:mb-[80px] tablet:mb-[123px]">
       <div className="w-[65px] md:w-[63px] lg:w-[145px] h-full flex flex-col items-center justify-center">
-        <div className="w-full card-name bg-white border-[5px] tablet:border-[13px] border-third rounded-t-[81px] flex-grow flex flex-col justify-center">
-          <h5 className="text-4xl tablet:text-[64px] tablet:leading-[68px] text-primary pt-[21px] pb-[5px]">{cardName}</h5>
-        </div>
+        <h5 className="w-full card-name bg-white border-[5px] tablet:border-[13px] border-third rounded-t-[81px] flex-grow flex flex-col justify-center text-4xl tablet:text-[64px] tablet:leading-[68px] text-primary pt-[21px] pb-[5px]">{cardName}</h5>
         <div className="card-bottom w-[65px] md:w-[63px] lg:w-[145px] h-[29px] tablet:h-[48px] bg-third"></div>
       </div>
       <div className="flex flex-col">
